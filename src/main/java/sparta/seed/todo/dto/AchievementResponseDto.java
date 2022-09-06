0423d13a7cd1815c9d10829dfdc1e6ba1ac56abd
JaejunSagong <jaejun7407@naver.com>
2022-09-07 03:26:05 +0900

    [fix] AchievementResponseDto 변수명 수정

--- src/main/java/sparta/seed/todo/dto/AchievementResponseDto.java
+++ src/main/java/sparta/seed/todo/dto/AchievementResponseDto.java
@@ -1,18 +1,17 @@
 package sparta.seed.todo.dto;
 
-import com.querydsl.core.annotations.QueryProjection;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public class AchievementResponseDto {
-    private String AchievementRate;
+    private String achievementRate;
     private long totalCnt;
     private long completeCnt;
 
     @Builder
     public AchievementResponseDto(String achievementRate, long totalCnt, long completeCnt) {
-        AchievementRate = achievementRate;
+        this.achievementRate = achievementRate;
         this.totalCnt = totalCnt;
         this.completeCnt = completeCnt;
     }
--- src/main/java/sparta/seed/todo/service/TodoService.java
+++ src/main/java/sparta/seed/todo/service/TodoService.java
@@ -1,9 +1,6 @@
 package sparta.seed.todo.service;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.access.annotation.Secured;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import sparta.seed.exception.CustomException;
@@ -16,12 +13,6 @@ import sparta.seed.todo.dto.TodoResponseDto;
 import sparta.seed.todo.repository.TodoRepository;
 import sparta.seed.util.TimeCustom;
 
-import java.sql.Timestamp;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
 
 @RequiredArgsConstructor

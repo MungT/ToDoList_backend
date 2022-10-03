7e395d76f6ff4ee8c9f95096f30d10f77ec0491e
JaejunSagong <jaejun7407@naver.com>
2022-10-03 21:04:29 +0900

    [fix] import 수정

--- src/main/java/sparta/seed/config/JwtSecurityConfig.java
+++ src/main/java/sparta/seed/config/JwtSecurityConfig.java
@@ -6,8 +6,8 @@ import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import sparta.seed.login.jwt.JwtFilter;
-import sparta.seed.login.jwt.TokenProvider;
+import sparta.seed.jwt.JwtFilter;
+import sparta.seed.jwt.TokenProvider;
 
 // 직접 만든 TokenProvider 와 JwtFilter 를 SecurityConfig 에 적용할 때 사용
 @RequiredArgsConstructor
